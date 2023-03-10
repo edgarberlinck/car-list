5b1366dd19cc308817e5e6cdd1693f0f7aef98b6
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-03-10 16:31:53 +0100

    feat: learn and shop page

--- src/components/Atoms/CarImage/CarImage.tsx
+++ src/components/Atoms/CarImage/CarImage.tsx
@@ -1,15 +1,17 @@
 import { ImageLoader } from 'next/image'
-import Image from 'next/legacy/image' // Using legacy due some know bugs in other browsers
+import Image from 'next/legacy/image'
 
 type CarImagePropType = {
   src: string
   alt: string
+  width?: number
+  height?: number
 }
 
 const GCPBucketLoader: ImageLoader = ({ src }) => {
   return `${process.env.NEXT_PUBLIC_IMAGE_URL}${src}`
 }
 
-const CarImage: React.FC<CarImagePropType> = ({ src, alt }) => (<Image src={src} alt={alt} layout='intrinsic' width={370} height={300} loader={GCPBucketLoader} />)
+const CarImage: React.FC<CarImagePropType> = ({ src, alt, width = 370, height = 300 }) => (<Image src={src} alt={alt} layout='intrinsic' width={width} height={height} loader={GCPBucketLoader} />)
 
 export default CarImage
--- src/lib/cars.ts
+++ src/lib/cars.ts
@@ -7,3 +7,11 @@ export async function fetchCarList(): Promise<Car[]> {
   const fileContents = await fs.readFile(jsonDirectory + '/cars.json', 'utf8');
   return JSON.parse(fileContents)
 }
+
+export async function fetchCarById(id: string): Promise<Car | undefined> {
+  const jsonDirectory = path.join(process.cwd(), 'public', 'api');
+  const fileContents = await fs.readFile(jsonDirectory + '/cars.json', 'utf8');
+  const carList = JSON.parse(fileContents) as Car[]
+  
+  return Promise.resolve(carList.find((car: Car) => car.id === id))
+}
